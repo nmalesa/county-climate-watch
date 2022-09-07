08d03a19af381c03b7c275691f0a8cc3bd3ef7a5
Natalia Malesa <natalia@onlinemeded.org>
2022-09-07 09:49:04 -0500

    feat: Add species data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import FetchComponent from './components/fetchComponent.js';
 import MapChart from './components/mapChart.js'
+import SpeciesSelection from './pages/SpeciesSelection.js';
 
 const App = () => {
   return (
    <div>
-    <FetchComponent />
+    <SpeciesSelection />
    </div>
   );
 }
---
+++ src/pages/SpeciesSelection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SpeciesSelection = () => {
+    return (
+        <div>
+            Species Selection
+        </div>
+    );
+};
+
+export default SpeciesSelection;
