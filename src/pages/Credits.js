2e2e8d08189037e3c51c638c74f7e172745605f8
Natalia Malesa <natalia@onlinemeded.org>
2022-09-07 10:25:28 -0500

    fix: Display bird image

---
+++ src/pages/Credits.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Credits = () => {
+    return (
+        <div>
+            Hello, world!
+        </div>
+    );
+};
+
+export default Credits;
--- src/pages/SpeciesSelection.js
+++ src/pages/SpeciesSelection.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import species from '../data/species.json';
 
 const SpeciesSelection = () => {
     return (
-        <div>
-            Species Selection
-        </div>
+        < >
+            <h1>Species Selection</h1>
+            <img src={require("../assets/images/eabl.jpg")} alt={species[0].commonName} />
+        </>
     );
 };
 
