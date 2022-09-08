f00668a97aa9be0e64f7ab7a3b5e9c6af3ad1956
Natalia Malesa <natalia@onlinemeded.org>
2022-09-08 16:59:37 -0500

    fix: Export App code and install Prettier

---
+++ src/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "County Climate Watch")
+  );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
