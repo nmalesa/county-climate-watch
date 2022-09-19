0d6ad7b0b651030f44dcc015889e3a2bafed7ca9
Natalia Malesa <natalia@onlinemeded.org>
2022-09-19 15:40:19 -0500

    fix: Retrieve counties by state

--- src/components/SearchParams.js
+++ src/components/SearchParams.js
@@ -1,10 +1,20 @@
-import { useState } from "react";
-import{states} from "../data/states"
+import { useState, useEffect } from "react";
+import { states } from "../data/states";
 import counties from "../data/counties.json";
 
 const SearchParams = () => {
   const [state, setState] = useState("");
   const [county, setCounty] = useState("");
+
+  useEffect(() => {
+    console.log("State: ", state);
+    console.log("First county: ", counties[0]);
+    for (let idx = 0; idx < counties.length; idx++) {
+      if (counties[idx].State === state) {
+        console.log("Counties: ", counties[idx]);
+      }
+    }
+  }, [state]);
 
   return (
     <div className="search-params">
